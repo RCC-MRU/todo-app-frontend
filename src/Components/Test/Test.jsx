226f9c3da8ff764a3a9cd642707e7e29b96183d7
Sanchit <sanchit02@outlook.com>
2021-08-13 19:44:30 +0530

    folder structure refined

---
+++ src/Components/Test/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Test.css";
+
+const Test = () => {
+  return (
+    <div>
+      <h1>this is test file</h1>
+    </div>
+  );
+};
+
+export default Test;
---
+++ src/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Router = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default Router;
