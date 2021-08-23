0a12ea63b8a2b0e28bc78f0d8697464c4c665671
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2021-08-24 01:51:39 +0530

    login-page-basic structure

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import "./Login.css";
 const Login = () => {
   return (
     <div>
-      Login
+      <div class="container m-auto p-0 d-grid place-item-center ">
+        <div class=" white-box">
+          <div class="row main-row">
+            <div class="col-md-7 ">
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Username"
+                ></input>
+              </div>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Password"
+                ></input>
+              </div>
+              <button type="button" class="btn btn-primary m-auto px-5 rounded  d-flex justify-content-center">
+                Login
+              </button>
+            </div>
+            <div class="col-md-5">
+              <h1 id="heading">svg image</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
