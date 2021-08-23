ff49e9bdede1985bcb9d6a585b709cc71e9343c1
Arsh <arshlakhina2002@gmail.com>
2021-08-23 20:40:50 +0530

    Login in progress

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+
 const Login = () => {
   return (
-    <div>
-      
+    <React.Fragment>
+    <div className="container">
+      <div className="login-left">
+        <h2>Get Started</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi eius quidem in blanditiis quasi consequatur, laudantium officiis nam praesentium perspiciatis architecto itaque non, debitis facere exercitationem illum incidunt adipisci accusamus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi eius quidem in blanditiis quasi consequatur, laudantium off
+        </p>
+        <h2>Signup</h2>
+        <h2>Login</h2>
+      </div>
+
+      <div className="login-right">
+        <img src="https://images.unsplash.com/photo-1618588507085-c79565432917?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
+      </div>
     </div>
+    </React.Fragment>
   )
 }
 
