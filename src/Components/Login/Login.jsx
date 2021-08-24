6de77d2eac70d352d9214c3d9efb76af014bbf38
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2021-08-24 13:18:50 +0530

    svg done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,42 @@
 import React from "react";
 import "./Login.css";
+import { FaUserCircle } from "react-icons/fa";
+import { MdExitToApp } from "react-icons/md";
 const Login = () => {
   return (
     <div>
-      
+      <nav class="navbar navbar-expand-lg  bg-nav">
+        <a class="navbar-brand" href="#">
+          RCC TODO APP
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <div class="user-account">
+              <FaUserCircle size={35} />
+            </div>
+            <a class="nav-link text-white" id="" href="#">
+              Username
+            </a>
+            <div class="logout">
+              <MdExitToApp size={35} />
+            </div>
+            <a class="nav-link text-white" href="#">
+              Logout
+            </a>
+          </div>
+        </div>
+      </nav>
       <div class="container wrap ">
         <div class=" white-box">
           <div class="row main-row">
@@ -36,7 +69,7 @@ const Login = () => {
               </button>
             </div>
             <div class="col-md-5">
-              <h1 id="heading">svg image</h1>
+              <img class="login-image" src={process.env.PUBLIC_URL + "/images/loginImage.svg"} />
             </div>
           </div>
         </div>
