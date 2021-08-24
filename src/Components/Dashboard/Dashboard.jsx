9867cbd0c1a5ec71bbed3314559ebf09ffa221a5
Arsh <arshlakhina2002@gmail.com>
2021-08-25 00:30:53 +0530

    Added react icons but styling is left

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {BsCheckAll, BsListUl} from "react-icons/bs";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <React.Fragment>
+      <div class="container dashboard">
+        <div className="dashboard-left">
+          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt=""/>
+        </div>
+        <div className="dashboard-right">
+        <BsCheckAll/>
+        <BsListUl/>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
