99368cb80055411ed2768730841b921c1e61038f
Kunal Arora <10kunalji@gmail.com>
2021-09-12 19:54:44 +0530

    pwa

--- src/Components/SignupComponent.jsx
+++ src/Components/SignupComponent.jsx
@@ -3,52 +3,52 @@ import React from "react";
 const SignupComponent = () => {
   return (
     <React.Fragment>
-      <div class="container white-box">
-        <div class="row">
-          <div class="col-md-6 my-2">
+      <div className="container white-box">
+        <div className="row">
+          <div className="col-md-6 my-2">
             <h2 className="mb-3">Signup</h2>
-            <div class="form-group">
+            <div className="form-group">
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="fullname"
                 placeholder="Fullname"
               ></input>
             </div>
-            <div class="form-group">
+            <div className="form-group">
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="email"
                 placeholder="Email address"
               ></input>
             </div>
-            
-            <div class="form-group">
+
+            <div className="form-group">
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="username"
                 placeholder="Username"
               ></input>
             </div>
-            <div class="form-group">
+            <div className="form-group">
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="password"
                 placeholder="Password"
               ></input>
             </div>
 
-            <button type="submit" class="btn btn-blue-dark">
+            <button type="submit" className="btn btn-blue-dark">
               Signup
             </button>
             <a id="signup-already-acc" href="/login">
               Already have an account?
             </a>
           </div>
-          <div class="col-md-6 my-2">
+          <div className="col-md-6 my-2">
             <img
               src={process.env.PUBLIC_URL + "/images/signupImage.svg"}
               alt="signup"
