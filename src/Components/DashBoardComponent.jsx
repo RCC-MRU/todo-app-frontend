af226106d740ebc06065218ae10898580f6a8ebf
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2021-09-16 00:44:48 +0530

    dashboard page

--- src/Components/DashBoardComponent.jsx
+++ src/Components/DashBoardComponent.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 const DashBoardComponent = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <React.Fragment>
+      <div className="container white-box">
+        <div className="row">
+          <div className="col-md-8 m-0">
+            <img
+              src={process.env.PUBLIC_URL + "/images/dashBoard.svg"}
+              alt="Homepage"
+              className="img-fluid "
+              width="70%"
+              height= "70%"
+            />
+          </div>
+          <div className="col-md-4">
+            <button type="button" class="btn btn-primary">ADD</button>
+            <button type="button" class="btn btn-primary">Tick</button>
+            <button type="button" class="btn btn-primary ">LIST</button>
+        </div>
+      </div>
+      </div>
+    </React.Fragment>
   )
-}
+};
 
 export default DashBoardComponent
