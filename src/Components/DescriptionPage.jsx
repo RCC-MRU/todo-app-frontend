8c74929c8430b2ec70c77cc6e015656c3dd560cd
Arsh <arshlakhina2002@gmail.com>
2021-09-24 10:33:14 +0530

    Changed tha alignment of tags and date-time

--- src/Components/DescriptionPage.jsx
+++ src/Components/DescriptionPage.jsx
@@ -10,50 +10,66 @@ const DescriptionPage = () => {
       <div className="container">
         <div className="white-box">
           <div className="row">
-            <div className="description col-md-10">
+            <div className="description col-md-12">
               <div className="row">
-                <div className="title col-md-10 ">
-                  <input type="text" id="title" name="title"/>
+                <div className="title col-md-12">
+                  <input type="text" id="title" name="title" />
                 </div>
               </div>
               <div className="row">
-                <div className="tags col-md-10 ">
-                  <div className="important ">
-                    <AiFillStar size="30"/>
-                    <input type="button" value="Important" id=""/>
+                <div className="date-time-and-tags">
+                  <div className="time-and-date col-md-7">
+                    <div className="date">
+                      <label for="date"><strong>DATE</strong></label>
+                      <input type="date" id="date" name="date" />
+                    </div>
+                    <div className="time">
+                      <div className="start-time">
+                        <label for="starttime"><strong>START TIME</strong></label>
+                        <input type="time" id="starttime" name="starttime" />
+                      </div>
+                      <div className="end-time">
+                        <label for="endtime"><strong>END TIME</strong></label>
+                        <input type="time" id="endtime" name="endtime" />
+                      </div>
+                    </div>
                   </div>
-                  <div className="completed ">
-                    <TiTickOutline size="30" />
-                    <input type="button" value="Completed" id=""/>
+                  <div className="row">
+                    <div className="tags col-md-5">
+                      <div className="tags-alignment">
+                        <div className="important">
+                          <AiFillStar size="30" color="#4A7DFF"/>
+                          <input type="button" value="Important" id="" />
+                        </div>
+                        <div className="completed ">
+                          <TiTickOutline size="30" color="#4A7DFF" />
+                          <input type="button" value="Completed" id="" />
+                        </div>
+                        <div className="delete ">
+                          <MdDelete size="30" color="#4A7DFF" />
+                          <input type="button" value="Delete" id="" />
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="delete ">
-                    <MdDelete size="30" />
-                    <input type="button" value="Delete" id=""/>
-                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="description-of-task col-md-12">
+                  <textarea
+                    name="description"
+                    id="description"
+                    cols="137"
+                    rows="4"
+                    placeholder="Description"
+                  ></textarea>
                 </div>
               </div>
               <div className="row">
-                <div className="description-of-task col-md-10">
-                  <textarea name="description" id="description" cols="90" rows="6" placeholder="Description"></textarea>
-                </div>
-              </div>
-              <div className="row">
-                <div className="add-todo col-md-10">
-                  <GoPlus size="30" />
-                  <input type="button" value="Add ToDo" id=""/>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="time-and-date col-md-8">
-                <div className="date">
-                  <h5>20 Sept</h5>
-                </div>
-                <div className="start-time">
-                  <h5>9:00 AM</h5>
-                </div>
-                <div className="end-time">
-                  <h5>11:00AM</h5>
+                <div className="add-todo col-md-12">
+                  <GoPlus size="30" color="#4A7DFF"/>
+                  <input type="button" value="Add ToDo" id="" />
                 </div>
               </div>
             </div>
